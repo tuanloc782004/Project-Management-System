514f3a34ec7210fa900d1787fa877f0f3a311063
Yume Rokku <163641570+tuanloc782004@users.noreply.github.com>
2025-04-11 12:15:00 +0700

    spring security 1st

---
+++ projectmanagementsystem/src/test/java/com/tuanloc/ProjectmanagementsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuanloc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectmanagementsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
